c2b98bd4ab87c3c8a0d636c0488fb75cf9c86d37
Rudney Rodrigues <rudney.un2016@gmail.com>
2022-06-28 11:53:49 -0300

    Styling to the 404 error page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,36 +5,37 @@ import { SmileySad } from "phosphor-react";
 
 const PageNotFound: NextPage = () => {
   return (
-    <Flex flex="1" flexDir="column" gap="1rem" justify="center" align="center" minH="100vh" px="1rem">
-      <Text color="red.500" fontSize={{
-        base: "5xl",
-        lg: "7xl",
-        xl: "9xl",
-      }}>
-        404
-      </Text>
-
-      <Flex align="center" justify="center" gap="1rem" flexDir={{
-        base: "column",
-        lg: "row",
-      }}>
-        <Text textAlign="center" fontSize={{
-          base: "xl",
-          lg: "2xl",
+    <Flex flex="1" flexDir="column" gap="1rem" justify="center" align="center" minH="100vh" px="1rem" backgroundImage="url('/images/blur-background.png')" backgroundRepeat="no-repeat" backgroundSize="cover" backgroundPosition="top">
+      <Flex align="center" justify="center" gap="1rem" flexDir="column">
+        <Text bgGradient='linear(to-tr, blue.500, gray.900)' bgClip='text' fontWeight="bold" fontSize={{
+          base: "5xl",
+          lg: "7xl",
+          xl: "9xl",
         }}>
-          É uma pena, mas a pagina digitada não foi encontrada.
+          Oops!
         </Text>
 
-        <SmileySad weight="fill" size={40} />
+        <Text fontWeight="bold" textAlign="center" color="green.500" fontSize={{
+        base: "xl",
+        lg: "2xl",
+      }}>
+        404 - Página não encontrada
+      </Text>
       </Flex>
 
-      <Text textAlign="center" >
-        Retorne a pagina inicial e tente novamente
+      <Text maxW="500px" textAlign={{
+        base: "justify",
+        sm: "center",
+      }}>
+        A página que você está procurando pode ter sido removida devido a mudança de nome ou está temporariamente indisponível
       </Text>
 
       <Link href="/" passHref>
-        <ChakraLink>
-          Pagina Inicial
+        <ChakraLink py="1rem" px="1.5rem" mt="1rem" bgColor="green.500" borderRadius="full" fontWeight="bold" fontSize="sm" textTransform="uppercase" _hover={{
+          textDecoration: "none",
+          bgColor: "green.700",
+        }}>
+          Retornar a página inicial
         </ChakraLink>
       </Link>
     </Flex>
